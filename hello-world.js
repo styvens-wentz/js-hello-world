b6b950a5624d157568560755295ba6807a9f1c10
styvens59 <56022040+styvens59@users.noreply.github.com>
2019-12-11 14:45:35 +0100

    init

---
+++ hello-world.js
@@ -0,0 +1,17 @@
+//
+// This is a stub file for the 'Hello World' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+// Make sure to look at hello-world.spec.js--that should give you some hints about what is
+// expected here.
+
+'use strict';
+
+var HelloWorld = function() {};
+
+HelloWorld.prototype.hello = function(input) {
+//
+// YOUR CODE GOES HERE
+//
+};
+
+module.exports = HelloWorld;
